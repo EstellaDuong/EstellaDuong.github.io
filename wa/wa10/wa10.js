5152f3d4c5edb51c220b7922860d1159b8b00d0d
EstellaDuong <esdu4090@colorado.edu>
2024-03-21 10:48:35 -0600

    wa10

--- wa/wa10/wa10.js
+++ wa/wa10/wa10.js
@@ -6,7 +6,7 @@ const story = document.querySelector('.story');
 
 const storyText = "It was 100 fahrenheit outside, much too hot for :insertx:'s comfort. Bob gave them a magic stone, so :insertx: wished to be transported to an icy tundra. However, they ended up at :inserty: instead. While trying to go back, they :insertz:. Bob saw everything through their magic orb. Riddled with guilt, Bob sent their 9,000 pound platypus-bear to rescue :insertx:.";
 const insertX = ["Willy Wonka", "Choi Soobin", "The Grinch", "Jenna Ortega"];
-const insertY = ["the soup kitchen", "the Grand Canyon", "a Taylor Swift concert", "some calculus class"];
+const insertY = ["an outhouse on Mars", "the Grand Canyon", "a Taylor Swift concert", "some calculus class"];
 const insertZ = ["accidentally summoned the wrath of 1,000 dragons", "turned to dust", "rolled away down a hill", "fell into a chasm of darkness", "stopped for a snack and ate moldy cheese"];
 
 function randomValueFromArray(array){
