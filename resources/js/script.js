f097f30201d38bb866a073b4c52e5d2d92cdb5d7
Estella Duong <esdu4090@colorado.edu>
2026-07-23 17:03:32 -0600

    scroll to top

--- resources/js/script.js
+++ resources/js/script.js
@@ -51,6 +51,10 @@ document.getElementById('scroll-down-btn')?.addEventListener('click', () => {
     document.querySelector('.intro-container').scrollIntoView({ behavior: 'smooth' });
 });
 
+document.getElementById('scroll-top-btn')?.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
 
 
 
